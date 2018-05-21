1ceb394a5ac9269c28af7aef014b402901ce9c2f
rish-16 <mail.rishabh.anand@gmail.com>
2018-05-21 23:44:54 +0800

    1.2: Added generated images

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from tqdm import tqdm
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
@@ -10,24 +11,23 @@ from keras.optimizers import Adam
 from keras.utils import to_categorical
 np.random.seed(0)
 
-random_input_dim = 100
-
 class CatGAN():
     def __init__(self, lr, beta, summaries):
         self._lr = lr
         self._beta = beta
         self._model_summaries = summaries
+        self._random_input_dim = 100
         
     def load_generator(self):
         G = Sequential()
-        G.add(Dense(256, input_dim=random_input_dim, kernel_initializer=initializers.RandomNormal(stddev=0.02)))
+        G.add(Dense(256, input_dim=self._random_input_dim, kernel_initializer=initializers.RandomNormal(stddev=0.02)))
         G.add(LeakyReLU(0.3))
         G.add(Dense(512))
         G.add(LeakyReLU(0.3))
         G.add(Dense(1024))
-        G.add(LeakyReLU(0.3))
+        G.add(LeakyReLU(0.2))
         G.add(Dense(2048))
-        G.add(LeakyReLU(0.3))
+        G.add(LeakyReLU(0.2))
         G.add(Dense(784, activation='tanh'))
 
         G.compile(loss='binary_crossentropy', optimizer=Adam(lr=self._lr, beta_1=self._beta))
@@ -57,7 +57,7 @@ class CatGAN():
 
     def load_GAN(self, discriminator, generator, random_input_dim):
         discriminator.trainable = False
-        catgan_input = Input(shape=[random_input_dim,])
+        catgan_input = Input(shape=[self._random_input_dim,])
         x = generator(catgan_input)
         catgan_output = discriminator(x)
         catgan = Model(inputs=catgan_input, outputs=catgan_output)
@@ -68,14 +68,14 @@ class CatGAN():
 
     def save_generated_images(self, epoch, generator, show_images=False):
         num_examples = 100
-        random_noise = np.random.normal(0, 1, size=[num_examples, random_input_dim])
+        random_noise = np.random.normal(0, 1, size=[num_examples, self._random_input_dim])
         generated_images = generator.predict(random_noise)
-        generated_images = generated_images.reshape(examples, 28, 28)
+        generated_images = generated_images.reshape(num_examples, 28, 28)
 
         plt.figure(figsize=(10, 10))
         for i in range(generated_images.shape[0]):
             plt.subplot(10, 10, i+1)
-            plt.imshow(generated_images[i], cmap='gray')
+            plt.imshow(generated_images[i], cmap='gray_r')
             plt.axis('off')
 
         plt.tight_layout()
@@ -88,13 +88,12 @@ class CatGAN():
 
         generator = self.load_generator()
         discriminator = self.load_discriminator()
-        catgan = self.load_GAN(discriminator, generator, random_input_dim)
+        catgan = self.load_GAN(discriminator, generator, self._random_input_dim)
 
         for epoch in range(1, num_epochs + 1):
-            print ('------------Epoch {}------------'.format(epoch))
-            for i in (range(batch_count)):
+            for i in tqdm(range(batch_count), desc="Epoch {}".format(epoch)):
                 
-                random_noise = np.random.normal(0, 1, size=[batch_size, random_input_dim])
+                random_noise = np.random.normal(0, 1, size=[batch_size, self._random_input_dim])
                 image_batch = x_train[np.random.randint(0, x_train.shape[0], size=batch_size)]
 
                 generated_images = generator.predict(random_noise)
@@ -106,12 +105,12 @@ class CatGAN():
                 discriminator.trainable = True
                 discriminator.train_on_batch(X_discriminator, y_discriminator)
 
-                random_noise = np.random.normal(0, 1, size=[batch_size, random_input_dim])
+                random_noise = np.random.normal(0, 1, size=[batch_size, self._random_input_dim])
                 y_generator = np.ones(batch_size)
                 discriminator.trainable = False
                 catgan.train_on_batch(random_noise, y_generator)
 
-            if num_epochs == 1 or epoch % 20 == 0:
+            if epoch == 1 or epoch % 20 == 0:
                 self.save_generated_images(epoch, generator)        
 
     def load_cats(self):
@@ -128,10 +127,10 @@ class CatGAN():
             x_train = x_train.reshape(x_train.shape[0], 784)
 
             return (x_train, y_train, x_test, y_test)  
-          
+
         except Exception as e:
             print (e)            
 
-catgan = CatGAN(lr=0.0002, beta=0.5, summaries=False)
+catgan = CatGAN(lr=0.0002, beta=0.5, summaries=True)
 (x_train, y_train, x_test, y_test) = catgan.load_cats()
 catgan.train_gan(x_train, 200, 256)
